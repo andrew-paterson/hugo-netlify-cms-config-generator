b61367c93f02bb2549110981e81f8c094a3c6035
Andrew Paterson <andrew.paterson9@gmail.com>
2022-03-22 09:45:38 +0200

    Only send frontmatter to yml converter, process multiple single pages, and allow field customisations

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const functions = require('./exports');
 const path = '/home/paddy/development/luddite/content';
-// const path = '/home/paddy/hyraxbio/static-sites/hyraxbio.com-v2/content';
 
-functions.run(path);
+functions.run(path, {
+  keyMappings: [{
+    keys: ['date', 'publishdate'],
+    field: {
+      widget: 'datetime',
+      date_format: 'YYYY-MM-DD',
+      time_format: 'HH:mm:ss',
+      format: 'YYYY-MM-DD',
+    }
+  }, {
+    keys: ['cover_image'],
+    field: {
+      name: 'cover_image',
+      widget: 'image',
+      required: true,
+      media_folder: '/static/media/images/wines',
+      public_folder: '/media/images/wines'
+    }
+  }]
+});
