988777abde7599a2b5b9bbc0d4f21550beb916cc
Andrew Paterson <andrew.paterson9@gmail.com>
2022-09-04 11:31:24 +0200

    Defaultrs for fields and colelctions working

--- index.js
+++ index.js
@@ -1,40 +1,132 @@
-const functions = require('./exports');
-const path = '/home/paddy/development/luddite/content';
+const lib = require('node-sundries');
+const fs   = require('fs');
+const yaml = require('js-yaml');
+const merge = require('lodash.merge');
+const path = require('path');
 
-functions.run(path, {
-  keyMappings: [{
-    keys: ['date', 'publishdate'],
-    field: {
-      widget: 'datetime',
-      date_format: 'YYYY-MM-DD',
-      time_format: 'HH:mm:ss',
-      format: 'YYYY-MM-DD',
+module.exports = {
+  run(opts) {
+    const files = lib.getFiles(opts.source, false);
+    const dirs = lib.getDirs(opts.source, false);
+    const final = {
+      collections: [{
+        name: 'single-pages',
+        label: 'Single pages',
+        files: files.map(file => this.doFile(file, opts))
+      }]
+    };
+    dirs.forEach(dir => {
+      const collection = {
+        name: path.basename(dir),
+        label: lib.capitaliseFirstChar(path.basename(dir).replace(/_/g, ' ').replace('-', ' ')),
+        folder: `content/${path.basename(dir)}`,
+      };
+      const combinedFrontmatterJS = {};
+      lib.getFiles(dir).forEach(file => {
+        merge(combinedFrontmatterJS, this.getFrontMatter(file));
+      });
+      collection.fields = this.generateFieldsFromFrontmatter(combinedFrontmatterJS, opts).concat(this.defaultFields(dir, opts));
+      this.collectionTransforms(collection, opts);
+      final.collections.push(collection);
+    });
+    fs.writeFileSync(opts.dest, yaml.dump(final, opts.jsYamlOptions));
+  },
+
+  collectionTransforms(collection, opts) {
+    const defaults = opts.collectionDefaults.filter(obj => {
+      return (obj.paths || []).indexOf(collection.folder) > -1;
+    });
+    defaults.forEach(obj => {
+      collection = merge(collection, obj.props);
+    });
+    for (const key in collection) {
+      this.applyPropFunction(collection, key, [collection, opts]);
     }
-  }, {
-    keys: ['cover_image'],
-    field: {
-      name: 'cover_image',
-      widget: 'image',
-      required: true,
-      media_folder: '/static/media/images/wines',
-      public_folder: '/media/images/wines'
+  },
+
+  applyPropFunction(obj, key, functionArgs) {
+    if (typeof obj[key] === 'function') {
+      const newValue = obj[key](...functionArgs);
+      obj[key] = newValue;
     }
-  }, {
-    keys: ['type'],
-    field: {
-      name: 'type',
-      widget: 'hidden',
-      default: function(fieldName, opts) {
-        return fieldName;
+  },
+
+  defaultFields(file, opts) {
+    return (opts.defaultFields || []).filter(defaultField => {
+      return (defaultField.exclude || []).indexOf(path.basename(file, '.md'));
+    }).map(defaultField => defaultField.field);
+  },
+
+  doFile(file, opts) {
+    const frontmatterJS = this.getFrontMatter(file);
+    const defaultFields = this.defaultFields(file, opts);
+    return {
+      label: lib.capitaliseFirstChar(lib.basenameNoExt(file).replace(/_/g, ' ')),
+      name: lib.basenameNoExt(file),
+      file: `/content/${path.basename(file)}`,
+      fields: this.generateFieldsFromFrontmatter(frontmatterJS, opts).concat(defaultFields)
+    };
+  },
+
+  generateFieldsFromFrontmatter(frontmatterJS, opts) {
+    const parseLevel = (obj) => {
+      const fields = [];
+      for (const key in obj) {
+        let widgetType;
+        if (Array.isArray(obj[key])) {
+          widgetType = 'list';
+        } else if (obj[key] === null) {
+          widgetType = 'string';
+        } else {
+          widgetType = typeof obj[key];
+        }
+        let field = {
+          label: lib.capitaliseFirstChar(key.replace(/_/g, ' ')),
+          name: key,
+          widget: widgetType,
+          required: false
+        };
+        const keyMappings = opts.keyMappings || [];
+        const match  = keyMappings.find(item => item.keys.indexOf(field.name) > -1);
+       
+        if (match) {
+          field = merge(field, match.field);
+          for (const key in field) {
+            this.applyPropFunction(field, key, [frontmatterJS, opts]);
+          }
+          // for (const key in matchField) {
+          //   if (typeof matchField[key] === 'function') {
+          //     console.log(key);
+          //     const newValue = matchField[key](frontmatterJS, opts);
+          //     matchField[key] = newValue;
+          //   }
+          // }
+
+        }
+        if (widgetType === 'object') {
+          field['fields'] = parseLevel(obj[key]);
+        } else if (widgetType === 'list') {
+          const childPojos = obj[key].filter(item => lib.isPojo(item));
+          if (childPojos.length) {
+            field.summary = '{{fields.name}}';
+            const mergedObjects = childPojos.reduce((item, acc) => {
+              return merge(item, acc);
+            }, {});
+         
+            field['fields'] = parseLevel(mergedObjects);
+          }
+        }
+        fields.push(field);
       }
-    }
-  }],
-  defaultFields: [{
-    field: {
-      name: 'body',
-      label: 'Body',
-      widget: 'markdown'
-    }, 
-    exclude: ['importers']
-  }]
-});
+      return fields;
+    };
+    return parseLevel(frontmatterJS);
+  },
+
+  getFrontMatter(file) {
+    const fileContents = fs.readFileSync(file, 'utf8');
+    const firstLine = fileContents.split(/\r?\n/)[0].trim();    
+    const frontMatter = fileContents.split(firstLine)[1];
+    return (lib.yamlToJs(frontMatter) || [])[0];
+  }
+};
